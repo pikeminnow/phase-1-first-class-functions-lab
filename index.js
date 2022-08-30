6082e53f3e1067f825dbfa01dd87bce8409b44f5
Ashton MacKenzie <pikeminnow@gmail.com>
2022-08-29 21:12:50 -0400

    commit tree cdf1b4a5454bbcc04f1c25d66b1ad5b443a8167f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) =>  drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return (fares) => fares * multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, fn){
+    return fn(arrayOfDrivers);
+}
